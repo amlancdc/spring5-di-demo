705fb98641cf40a5328e87b688cfa66680fa9f50
Amlan Chowdhury <amlan.cdc@gmail.com>
2020-08-18 20:26:01 +0600

    Init

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -2,13 +2,11 @@ package guru.springframework.config;
 
 import guru.springframework.examlplebeans.FakeDataSource;
 import guru.springframework.examlplebeans.FakeJmsBroker;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.annotation.PropertySources;
 
 @Configuration
 //@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})
@@ -17,8 +15,6 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
         @PropertySource("classpath:jms.properties")
 })
 public class PropertyConfig {
-
-    private final Environment env;
 
     @Value("${guru.username}")
     String user;
@@ -38,17 +34,10 @@ public class PropertyConfig {
     @Value("${guru.jms.dburl}")
     String jmsDburl;
 
-    public PropertyConfig(Environment env) {
-        this.env = env;
-    }
-
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
         fakeDataSource.setUser(user);
-        System.out.println(fakeDataSource.getUser());
-        fakeDataSource.setUser(env.getProperty("guru.username"));
-        System.out.println(fakeDataSource.getUser());
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
 
@@ -64,10 +53,5 @@ public class PropertyConfig {
 
         return fakeJmsBroker;
     }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties(){
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;
-    }
+    
 }
